33e9dcdb0ad970a145e3d8a0e1111809ebd0a95a
Yiu Ming Wong <56940477+DexterW1@users.noreply.github.com>
2022-10-24 18:22:05 -0700

    Cleaned up code for easier find of functions. Added expand function that will check every possible solution.

--- Node.h
+++ Node.h
@@ -1,73 +1,44 @@
 //
 // Created by Dexter on 10/23/2022.
 //
-
 #ifndef EIGHT_PUZZLE_NODE_H
 #define EIGHT_PUZZLE_NODE_H
 #include <iostream>
 #include "bits/stdc++.h"
+#include "helpfunctions.h"
 using namespace std;
-const int N=3;
 class Node {
 public:
     Node(){
         h_of_n =0;
         depth = 0;
         g_of_n = 0;
+
     }
     Node(int puzzle[N][N]){
         h_of_n=0;
         depth=0;
         g_of_n=0;
         copyPuzzle(puzzle);
+
+    }
+    Node operator =(Node &rhs){
+        this->depth=rhs.depth;
+        copy(&this->nodePuzzle[0][0],&this->nodePuzzle[0][0]+ N*N,&rhs.nodePuzzle[0][0]);
+        this->h_of_n=rhs.h_of_n;
+        this->g_of_n=rhs.g_of_n;
+
     }
     int nodePuzzle [N][N];
+    Node *parent;
     int h_of_n;
     int depth;
     int g_of_n;
-    vector<pair<int,int>> findEmpty;
 
     //--------------------------Functions--------------------------------
     //This will copy the puzzle into the node class.
     void copyPuzzle(int puzzle[N][N]){
         copy(&puzzle[0][0],&puzzle[0][0]+ N*N,&nodePuzzle[0][0]);
-    }
-    //Used to find the zero and passes the row and col back by reference
-    bool find_zero(int& row, int& col){
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N;j++){
-                if(nodePuzzle[i][j]==0){
-                    row=i;
-                    col=j;
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-    //swaps the zero to the one above
-    void move_up(){
-        int i,j=0;
-        find_zero(i,j);
-        swap(nodePuzzle[i][j],nodePuzzle[i-1][j]);
-    }
-    //swaps the zero to the one below
-    void move_down(){
-        int i,j=0;
-        find_zero(i,j);
-        swap(nodePuzzle[i][j],nodePuzzle[i+1][j]);
-    }
-    //swaps the zero to the one on the left
-    void move_left(){
-        int i,j=0;
-        find_zero(i,j);
-        swap(nodePuzzle[i][j],nodePuzzle[i][j-1]);
-    }
-    //swaps the zero to the one on the right
-    void move_right(){
-        int i,j=0;
-        find_zero(i,j);
-        swap(nodePuzzle[i][j],nodePuzzle[i][j+1]);
     }
 
 };
--- main.cpp
+++ main.cpp
@@ -1,154 +1,71 @@
 #include <iostream>
-#include "bits/stdc++.h"
-#include <algorithm>
 #include "Node.h"
+#include "helpfunctions.h"
 using namespace std;
-//Global Variables
-int user_puzzle[N][N];
 
-//Quick Test puzzles
-int trivial [N][N]= {{1,2,3},
-                     {4,5,6},
-                     {7,8,0}};
-
-int veryEasy[N][N]={{1,2,3},
-                    {4,5,6},
-                    {7,0,8}};
-
-int easy[N][N]{{1,2,0},
-               {4,5,3},
-               {7,8,6}};
-
-int doable[N][N]{{0,1,2},
-                 {4,5,3},
-                 {7,8,6}};
-
-int oh_boy[N][N]{{8,7,1},
-                 {6,0,2},
-                 {5,4,3}};
-
-int impossible[N][N]{{8,6,7},
-                 {2,5,4},
-                 {3,0,1}};
-
-int eight_goal_state[N][N]{{1,2,3},
-                           {4,5,6},
-                           {7,8,0}};
-
-void print_intro(){
-    cout<<"Welcome to my 8-Puzzle Solver. Type '1'to use a default puzzle, or '2' to create your own."<<endl;
-}
-void print_CreateOwn(){
-    cout<<"Enter your puzzle, using a zero to represent a blank. Please only enter\nvalid 8-puzzles. Enter the puzzle demilimiting the numbers  with a space. Type\n"
-          "RETURN only when finished."<<endl;
-}
-void print_SelectAlgo(){
-    cout<<"Select Algorithm. (1) for Uniform Cost Search, (2) for the Misplaced Tile Heuristic, or (3) the Manhattan Distance Heuristic."<<endl;
-}
-void print_Puzzle(int puzzle[N][N]){
-    for(int i=0; i<N;i++){
-        for(int j=0; j<N;j++){
-            if(j==0){
-                cout<<"[";
-            }
-            cout<<puzzle[i][j];
-            if(j==2){
-                cout<<"]";
-            }
-            else{
-                cout<<",";
+vector<Node> expand(int puzzle[N][N]) {
+    int temp_array[N][N];
+    int row, col = 0;
+    find_zero(puzzle, row, col);
+    vector<Node> list;
+    for (int i = 0; i < 4; i++) {
+        copy(&puzzle[0][0], &puzzle[0][0] + N * N, &temp_array[0][0]);
+        //up
+        if (row > 0) {
+            if (i == 0) {
+                move_up(temp_array);
+                Node temp(temp_array);
+                list.push_back(temp);
             }
         }
-        cout<<endl;
-    }
-}
-void general_search(int puzzle[N][N],int heuristic){}; //work on
-void init_default_puzzle(){
-    int choice;
-    cout<<"You wish to use a default puzzle. Please enter a desired difficulty on a scale from 0 to 5."<<endl;
-    cin>>choice;
-    if(choice == 0){
-        copy(&trivial[0][0],&trivial[0][0]+ N*N,&user_puzzle[0][0]);
-    }
-    else if(choice == 1){
-        copy(&veryEasy[0][0],&veryEasy[0][0]+ N*N,&user_puzzle[0][0]);
-    }
-    else if(choice == 2){
-        copy(&easy[0][0],&easy[0][0]+ N*N,&user_puzzle[0][0]);
-    }
-    else if(choice == 3){
-        copy(&doable[0][0],&doable[0][0]+ N*N,&user_puzzle[0][0]);
-    }
-    else if(choice == 4){
-        copy(&oh_boy[0][0],&oh_boy[0][0]+ N*N,&user_puzzle[0][0]);
-    }
-    else if(choice == 5){
-        copy(&impossible[0][0],&impossible[0][0]+ N*N,&user_puzzle[0][0]);
-    }
-    else{
-        cout<<"Error Not a valid number \n";
-    }
-}
-void user_input_puzzle(){
-    string puzzle_row_one, puzzle_row_two,puzzle_row_three;
-    cin.ignore();
-    cout<<"Enter the first row: ";
-    getline(cin,puzzle_row_one);
-    cout<<endl;
-    cout<<"Enter the second row: ";
-    getline(cin,puzzle_row_two);
-    cout<<endl;
-    cout<<"Enter the three row: ";
-    getline(cin,puzzle_row_three);
-    cout<<endl;
-    for(int i=0; i<N;i++){
-        for(int j=0;j<N;j++){
-            if(i==0) {
-                remove(puzzle_row_one.begin(),puzzle_row_one.end(),' ');
-                user_puzzle[i][j] = puzzle_row_one[j] - '0';
+        //down
+        if (row < 2) {
+            if (i == 1) {
+                move_down(temp_array);
+                Node temp(temp_array);
+                list.push_back(temp);
             }
-            else if(i==1){
-                remove(puzzle_row_two.begin(),puzzle_row_two.end(),' ');
-                user_puzzle[i][j] = puzzle_row_two[j] - '0';
+        }
+        //left
+        if (col > 0) {
+            if (i == 2) {
+                move_left(temp_array);
+                Node temp(temp_array);
+                list.push_back(temp);
             }
-            else if(i==2){
-                remove(puzzle_row_three.begin(),puzzle_row_three.end(),' ');
-                user_puzzle[i][j] = puzzle_row_three[j] - '0';
+        }
+        //right
+        if (col < 2) {
+            if (i == 3) {
+                move_right(temp_array);
+                Node temp(temp_array);
+                list.push_back(temp);
             }
         }
     }
+    return list;
 }
-void select_and_init_algo(){
-    int choice=0;
-    print_SelectAlgo();
-    cin>>choice;
-    if(choice == 1){
-        general_search(user_puzzle,0);
-    }
-    else if(choice == 2){
-        general_search(user_puzzle,1);
-    }
-    else if(choice == 3){
-        general_search(user_puzzle,2);
-    }
+void general_search(){
+
 }
 int main() {
-    int puzzle_mode =0;
-    string puzzle_row_one, puzzle_row_two,puzzle_row_three;
+    int array[N][N];
+    int puzzle_mode = 0;
+    vector<Node> list;
+    string puzzle_row_one, puzzle_row_two, puzzle_row_three;
     print_intro();
-    cin>>puzzle_mode;
-    if(puzzle_mode==1){
+    cin >> puzzle_mode;
+    if (puzzle_mode == 1) {
         init_default_puzzle();
         select_and_init_algo();
     }
-    if(puzzle_mode==2){
+    if (puzzle_mode == 2) {
         print_CreateOwn();
         user_input_puzzle();
         select_and_init_algo();
     }
     Node test(user_puzzle);
     print_Puzzle(test.nodePuzzle);
-
 }
 //How to copy array to another
 //copy(&trivial[0][0],&trivial[0][0]+ N*N,&user_puzzle[0][0]);
